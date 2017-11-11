bbdb28cef0a3e5a4438641925cfe5a127abf589e
FreelanceApps <freelanceapps16@gmail.com>
2017-11-11 15:57:31 +0200

    Forgot to add some scripts on git.They are added now.

---
+++ Assets/Plan1 - 3 accounts/Scripts/Plan1 MVA/Adapter-Views/Other/NewTransactionPanelView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewTransactionPanelView : MonoBehaviour
+{
+    public Text transactionAmountText;//Forced to display numbers only
+    public Text transactionDescriptionText;//All characters
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
