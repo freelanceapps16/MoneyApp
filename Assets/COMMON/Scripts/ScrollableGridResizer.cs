3818270bbafd1de38a6ab58b0aaee593eecb0490
FreelanceApps <freelanceapps16@gmail.com>
2018-02-28 20:40:15 +0200

    Added one more option in the "Options" panel,as a shopping list creator. The panel is NOT ready.
    Created the UniformUITextBestFit script,to set the same common Text controls font size from an array of Text controls.
    By default,it keeps the Text controls content single-lined,but it can accept multi-lined text content too.

---
+++ Assets/COMMON/Scripts/ScrollableGridResizer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollableGridResizer : MonoBehaviour
+{
+    public float childItemsHeight;
+    public int childItemsCount;
+    public float spacing;
+    public float paddingTop;
+    public float paddingBottom;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ResizeUsingCurrentInfo()
+    {
+        Vector2 currentSizeDelta = gameObject.GetComponent<RectTransform>().sizeDelta;
+
+        float totalHeight = (childItemsHeight * childItemsCount) + ((childItemsCount - 1) * spacing) + paddingTop + paddingBottom;
+
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(currentSizeDelta.x, totalHeight);
+
+    }
+
+}
---
+++ Assets/COMMON/Scripts/UniformUITextBestFit.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic
+    ;
+/*
+    Usage :
+    1.  Add Text components inside the array in editor
+    2.  Set for each Text component the best-fit option
+    3.  You can chech the "acceptMultiLineTexts" if you want to have multi-line texts in your UI controls.
+        Otherwise,the font size will be again decreased,to fit all texts in a single line.
+
+     */
+
+public class UniformUITextBestFit : MonoBehaviour {
+
+    public List<Text> textList;
+    public bool acceptMultiLineTexts = false;//to decrease font even more,until all texts are single line
+    public int computedMinimmumSizeResult = 0;//Just to display the computed size in Editor
+
+    // Use this for initialization
+    void Start ()
+    {
+        ComputeTextsCommonSize();
+    }
+
+    public void ComputeTextsCommonSize()
+    {
+        //Set the maximmum font size to fit. Useful for high resolution devices.
+        foreach (Text item in textList)
+            item.resizeTextMaxSize = 300;
+
+        StartCoroutine(SetMinimmumCommonSize());
+    }
+
+    private IEnumerator SetMinimmumCommonSize()
+    {
+        //Wait a frame,for the Engine to compute controls size
+        yield return null;
+
+        computedMinimmumSizeResult = textList[0].cachedTextGenerator.fontSizeUsedForBestFit;
+
+        foreach (Text item in textList)
+        {
+            if (item.cachedTextGenerator.fontSizeUsedForBestFit < computedMinimmumSizeResult)
+                computedMinimmumSizeResult = item.cachedTextGenerator.fontSizeUsedForBestFit;
+        }
+
+        ApplyTextSize(computedMinimmumSizeResult);
+        yield return null;//wait a frame
+
+        if (!acceptMultiLineTexts)
+        {
+            while (ContainsMultilineTexts())
+            {
+                computedMinimmumSizeResult--;
+                ApplyTextSize(computedMinimmumSizeResult);
+            }
+        }
+
+        yield return null;
+    }
+
+    private bool ContainsMultilineTexts()
+    {
+        Canvas.ForceUpdateCanvases();
+        foreach (Text item in textList)
+        {
+            if (item.cachedTextGenerator.lineCount > 1)
+                return true;
+        }
+
+        return false;
+    }
+
+    private void ApplyTextSize(int size)
+    {
+        foreach (var item in textList)
+            item.resizeTextMaxSize = size;
+    }
+
+}
